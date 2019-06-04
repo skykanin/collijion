fbb6af0e83116e8cc012e088f7e111e275769bb0
skykanin <3789764+skykanin@users.noreply.github.com>
2019-06-04 22:59:39 +0200

    Fixed several issues with calculation function

--- src/collijion/planet.clj
+++ src/collijion/planet.clj
@@ -1,8 +1,8 @@
 (ns collijion.planet
   (:import [clojure.lang PersistentVector]))
 
-#_(def G 6.67264E-11)
-(def G 6.67E-9)
+(def G 6.67264E-11)
+#_(def G 1E-5)
 
 (defrecord Planet [m r x y ^PersistentVector v])
 
@@ -32,14 +32,6 @@
   (when-not (= n 0)
    (mapv #(/ % n) v)))
 
-(defn distance
-  "Calculates the Euclidean distance between
-  two points"
-  [p1 p2]
-  (Math/sqrt
-   (+ (Math/pow (- (:x p1) (:x p2)) 2)
-      (Math/pow (- (:y p1) (:y p2)) 2))))
-
 (defn magnitude
   "Calculates the magnitude of a vector"
   [[x y]]
@@ -49,7 +41,7 @@
   "Calculates the unit vector of a vector"
   [v]
   (let [m (magnitude v)]
-    (if (= m 0)
+    (if (= m (double 0))
       [0 0]
       (div v m))))
 
@@ -57,13 +49,13 @@
   "Calculates the attraction force between two
   bodies using Newton's law of universal gravitation.
   Force applied on object 2 exerted by object 1."
-  [{x1 :x y1 :y :as p1} {x2 :x y2 :y :as p2}]
-  (let [dist (distance p2 p1)
-        unit (div (sub [x2 y2] [x1 y1]) dist)]
-    (mult unit
-     (* (- G)
-        (/ (* (:m p1) (:m p2))
-           (Math/pow dist 2))))))
+  [{x1 :x y1 :y m1 :m :as p1} {x2 :x y2 :y m2 :m :as p2}]
+  (let [diff (sub [x2 y2] [x1 y1])
+        dist (magnitude diff) ; distance between the two bodies
+        unit (unit diff) ; unit vector (direction only)
+        const (/ (* G m1 m2)
+                 (Math/pow dist 2))]
+    (mult unit const)))
 
 (defn gen-f-vec
   "Calculate all force vectors for all objects in
@@ -72,27 +64,27 @@
   (partition (dec (count planets))
    (for [p1 planets p2 planets
          :when (not= p1 p2)]
-     (grav-vec p2 p1)))) ;check this application
+     (grav-vec p1 p2)))) ;check this application
 
 (defn apply-forces
   "Apply all the force vectors to the planets"
-  ([planets forces] (apply-forces planets forces 60))
-
-  ([planets forces timestep]
-   (let [seconds (/ timestep 1000)
-         sum-forces (fn [old fl]
-                      (add old (mult (reduce add fl) seconds)))
-         zip (fn [p force-list]
-               (update p :v sum-forces force-list))]
-    (map zip planets forces))))
+  ([planets forces]
+   (let [sum-forces (fn [old fl m]
+                      (as-> fl n
+                        (reduce add n)
+                        (div n m)
+                        (add old n)))
+         zip (fn [p fl]
+               (update p :v sum-forces fl (:m p)))]
+     (map zip planets forces))))
 
 (defn apply-speed
   "Applies the new velocity to the planets"
   [planets]
   (let [move (fn [{[vx vy] :v :as p}]
                (-> p
-                (update :x #(+ % vx))
-                (update :y #(+ % vy))))]
+                (update :x + vx)
+                (update :y + vy)))]
     (map move planets)))
 
 (defn update-planets
